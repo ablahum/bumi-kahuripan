ebdeba997bb25a9cc8a712080a4feb45ef702bc6
ablahum <mriztama@gmail.com>
2024-12-02 18:40:15 +0700

    initial commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+import { createApp } from "vue";
+import ExampleComponent from "./components/ExampleComponent.vue";
+
+createApp({
+    components: {
+        ExampleComponent,
+    },
+}).mount("#app");
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Hello, Vue with Vite!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ExampleComponent",
+};
+</script>
