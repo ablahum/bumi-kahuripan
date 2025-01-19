72bc63f3fe35bc189cfaf4ee00660197316983bc
ablahum <mriztama@gmail.com>
2025-01-20 00:58:33 +0700

    refacto

--- resources/js/apis/rooms.js
+++ resources/js/apis/rooms.js
@@ -1,21 +1,21 @@
-// import axios from "axios";
+import axios from "axios";
 
 export const getAll = async () => {
-    return await $axios.get("/rooms");
+    return await axios.get("/rooms");
 };
 
 export const getOne = async (id) => {
-    return await $axios.get(`/rooms/${id}`);
+    return await axios.get(`/rooms/${id}`);
 };
 
 export const createOne = async (payload) => {
-    return await $axios.post("/rooms", payload);
+    return await axios.post("/rooms", payload);
 };
 
 export const updateOne = async (payload) => {
-    return await $axios.put(`/rooms/${payload.id}`, payload);
+    return await axios.put(`/rooms/${payload.id}`, payload);
 };
 
 export const deleteOne = async (id) => {
-    return await $axios.delete(`/rooms/${id}`);
+    return await axios.delete(`/rooms/${id}`);
 };
--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,9 @@ import axios from "axios";
 import router from "./routes";
 
 axios.defaults.baseURL = "http://127.0.0.1:8000/api";
-// axios.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem(
-//     "auth-token"
-// )}`;
+axios.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem(
+    "auth-token"
+)}`;
 
 const app = createApp(App);
 
