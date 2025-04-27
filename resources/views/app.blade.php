5a2fcdd8c65077bfdab976267e7921f40ca9d0c0
ablahum <mriztama@gmail.com>
2025-04-28 00:23:21 +0700

    fix: rename project name

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BK - Point of Sales App</title>
-    @vite(['resources/js/app.js', 'resources/css/app.css'])
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bumi Kahuripan | Point of Sales App</title>
+  @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
+
 <body>
-    <div id="app"></div>
+  <div id="app"></div>
 </body>
+
 </html>
