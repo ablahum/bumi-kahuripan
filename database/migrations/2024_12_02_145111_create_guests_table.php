74da05265aba32b1afac70b1b4f467f5323a0043
ablahum <mriztama@gmail.com>
2025-05-01 00:49:10 +0700

    fix(migrations): adding identity_photo column for guests table

--- database/migrations/2024_12_02_145111_create_guests_table.php
+++ database/migrations/2024_12_02_145111_create_guests_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('origin');
             $table->string('phone');
+            $table->string('identity_photo');
         });
     }
 
