7f6f34d4fa2337cd83ab12a0157d1b697b762beb
ablahum <mriztama@gmail.com>
2025-05-07 16:48:32 +0700

    chore: removing migration code

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
 
-Route::get('/run-migrate', function () {
-  Artisan::call('migrate', ['--force' => true]);
-  return 'Migration completed.';
-});
 Route::get('/{any}', function () {
   return view('app');
 })->where('any', '.*');
