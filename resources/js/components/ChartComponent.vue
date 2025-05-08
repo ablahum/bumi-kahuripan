6309058e4c8c9e7a904bb0efc8504f39405fec75
ablahum <mriztama@gmail.com>
2025-05-09 05:33:49 +0700

    feat(dashboard): adding order stats with bar chart

---
+++ resources/js/components/ChartComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="overflow-x-auto">
+    <div class="min-w-[600px] min-h-[450px] w-full h-full">
+      <Bar :data="chartData" :options="chartOptions" />
+    </div>
+  </div>
+</template>
+
+
+<script>
+import { Bar } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'ChartComponent',
+  components: {
+    Bar
+  },
+  props: {
+    chartData: Object,
+    chartOptions: Object
+  }
+}
+</script>
--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -2,5 +2,12 @@ import TableComponent from './TableComponent.vue'
 import FormComponent from './FormComponent.vue'
 import FilterComponent from './FilterComponent.vue'
 import CardComponent from './CardComponent.vue'
+import ChartComponent from './ChartComponent.vue'
 
-export { TableComponent, FormComponent, FilterComponent, CardComponent }
+export {
+  TableComponent,
+  FormComponent,
+  FilterComponent,
+  CardComponent,
+  ChartComponent
+}
